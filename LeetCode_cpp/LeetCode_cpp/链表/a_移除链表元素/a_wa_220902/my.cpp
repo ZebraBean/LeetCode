37b278e5f8ae45920270a7e909500449b0be02c1
Zebra <1349889154@qq.com>
2022-09-04 11:22:51 +0800

    submit code

--- LeetCode_cpp/LeetCode_cpp/链表/a_移除链表元素/a_wa_220902/my.cpp
+++ LeetCode_cpp/LeetCode_cpp/链表/a_移除链表元素/a_wa_220902/my.cpp
@@ -1,13 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
 struct ListNode {
 	int val;
 	ListNode* next;
 	ListNode() : val(0), next(nullptr) {}
 	ListNode(int x) : val(x), next(nullptr) {}
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
+	
 };
 
 ListNode* removeElements(ListNode* head, int val) {
-	ListNode* dummyHead = new ListNode(0, head);
+	ListNode* dummyHead = new ListNode(0,head);
 	ListNode* p = dummyHead;
 	while (p->next != nullptr)
 	{
