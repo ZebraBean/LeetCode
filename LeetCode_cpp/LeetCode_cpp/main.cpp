7a1e086902a9b79ecd984d25ef393d1d069752bf
Zebra <1349889154@qq.com>
2022-09-06 23:35:58 +0800

    submit code

--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -3,22 +3,9 @@
 #include<string>
 using namespace std;
 int main() {
-	string s;
-	string t;
-	getline(cin, s);
-	getline(cin, t);
-
-	map<int, bool> mapS;
-	map<int, bool> mapT;
-	for (int i = 0; i < t.length(); i++) {
-		mapT[t[i]] = true;
-	}
-
-	for (string::iterator it = s.begin(); it != s.end(); it++) {
-		if (mapT[*it] == true) {
-			s.erase(it);
-			it--;
-		}
-	}
-	cout << s;
+	int a;
+	int b;
+	char c;
+	char d;
+	cout << ('a' < 'b');
 }
