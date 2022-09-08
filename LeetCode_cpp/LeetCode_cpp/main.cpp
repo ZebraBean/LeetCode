1c7aec5484904951a5e293ace128f7106f8aaaab
Zebra <1349889154@qq.com>
2022-09-08 21:24:04 +0800

    submit code

--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -4,41 +4,14 @@
 #include<algorithm>
 #include<string>
 using namespace std;
-int main() {
-	string s;
-	vector<string> a;
-	while (cin >> s)
+int main()
+{
+	struct a
 	{
-		int i = 0;
-		string temp;
-		while (i < s.length())
-		{
-			if (s[i] != ',')
-			{
-				temp += s[i];
-			}
-			else
-			{
-				a.push_back(temp);
-				temp = "";
-			}
-			i++;
-		}
-		a.push_back(temp);
-		sort(a.begin(), a.end());
-		for (int i = 0; i < a.size(); i++)
-		{
-			if (i != a.size() - 1)
-			{
-				cout << a[i] << ',';
-			}
-			else
-			{
-				cout << a[i] << endl;
-			}
-		}
-		a.clear();
-	}
-
+		short a;
+		int b;
+		double c;
+	};
+	cout << sizeof(a);
 	return 0;
 }
