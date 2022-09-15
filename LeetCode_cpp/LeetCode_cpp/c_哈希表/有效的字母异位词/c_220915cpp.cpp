193ab87702823b4a4dea312531a2d4b344288a6e
Zebra <1349889154@qq.com>
2022-09-15 23:13:12 +0800

    submit code

--- LeetCode_cpp/LeetCode_cpp/c_哈希表/有效的字母异位词/c_220915cpp.cpp
+++ LeetCode_cpp/LeetCode_cpp/c_哈希表/有效的字母异位词/c_220915cpp.cpp
@@ -5,6 +5,16 @@ public:
 	bool canConstruct(string ransomNote, string magazine) {
 		int hash[26] = { 0 };
 		for (auto c : ransomNote) {
+			hash[c - 'a']++;
 		}
+		for (auto c : magazine) {
+			hash[c - 'a']--;
+		}
+		for (auto i : hash) {
+			if (i > 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 };
--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 
 void test(int a[]) {
+	//这种方法是绝对错误的，数组在传参的时候是用指针传递的，此时的a是指针，而不是数组，范围是不确定的。
+	//范围for也没法遍历
 	for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
 		cout << a[i];
 	}
