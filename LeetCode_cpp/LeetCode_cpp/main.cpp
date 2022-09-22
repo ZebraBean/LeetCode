21f6fa2634c9a9f6824739339c009a0286e4e2bb
Zebra <1349889154@qq.com>
2022-09-22 23:55:55 +0800

    submit

--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -5,69 +5,21 @@
 #include <vector>
 #include <typeinfo>
 #include <assert.h>
-
 using namespace std;
-bool isHappy(int n) {
-	vector<int> a;
-	while (n != 1) {
-		long long temp = 0;
-		while (n > 0) {
-			int r = n % 10;
-			temp += r * r;
-			n /= 10;
-			cout << n << endl;
-		}
-		if (find(a.begin(), a.end(), temp) == a.end()) {//没找到
-			a.emplace_back(temp);
-			n = temp;
-			cout << temp << endl;
-		} else {
-			return false;
-		}
-	}
-	return 1;
+void Func(int a = 10, int b = 20, int c = 30) {
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
 }
-void test(int a[]) {
-	//这种方法是绝对错误的，数组在传参的时候是用指针传递的，此时的a是指针，而不是数组，范围是不确定的。
-	//范围for也没法遍历
-	for (int i = 0; i < sizeof(a) / sizeof(int); i++) {
-		cout << a[i];
-	}
-}
-void fun(int* a) {
-	cout << "int*";
-}
-void fun(int a) {
-	cout << "int";
-}
-
-class A {
-public:
-	A(int a = 10) {
-		this->a = a;
-	}
-	void print() {
-		cout << "aaaaaaa";
-	}
-private:
-	int a;
-};
-class B {
-public:
-	B() {
-		val = 15;
-	}
-private:
-	int val;
-	A a1;
-	A a2;;
-};
 int main() {
-	//int a = 0x01020304;
-	//int ans[5] = { 0 };
-	//B b1;
-	//int c;
-	//fun(0);
-	isHappy(2);
+	Func();
+	Func(1);
+	Func(1, 2);
+	Func(1, 2, 3);
+	//Func(, , 3);//不行
+	int* a = new int[1024 * 1024];
+	a[0] = 1;
+	cout << a[0] << endl << a[1];
+
 	return 0;
 }
