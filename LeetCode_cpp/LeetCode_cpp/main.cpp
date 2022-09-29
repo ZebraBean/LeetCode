dd182be456b8fbfc7f18d4d0beebc78304d96a16
Zebra <1349889154@qq.com>
2022-09-29 23:43:58 +0800

    submit code

--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -10,23 +10,9 @@ int cmp(int a, int b) {
 	return a > b;
 }
 int main() {
-	vector<int> a;
-	a.push_back(1);
-	a.push_back(2);
-	a.push_back(9);
-	a.push_back(4);
-	a.push_back(5);
-	a.push_back(6);
-	a.push_back(3);
+	string a;
 
-	for (auto num : a) {
-		cout << num << " " << endl;
-	}
-	cout << "ok";
-	sort(a.begin(), a.end(), cmp);
-	for (auto num : a) {
-		cout << num << " " << endl;
-	}
+	cout << a.capacity();
 
 	return 0;
 }
