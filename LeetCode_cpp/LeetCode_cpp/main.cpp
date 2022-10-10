e4389a94835bddec659a16f85fd18b7ff712870f
Zebra <1349889154@qq.com>
2022-10-10 22:45:07 +0800

    submit my code 22.10.10

--- LeetCode_cpp/LeetCode_cpp/main.cpp
+++ LeetCode_cpp/LeetCode_cpp/main.cpp
@@ -6,32 +6,6 @@
 #include <typeinfo>
 #include <assert.h>
 using namespace std;
-class B {
-public:
-	B() : b(4) {
-	}
-	void funB() {
-		//cout << typeid(this).name() << endl;
-	}
-private:
-	int b;
-};
-
-class A {
-public:
-	A() :b(B()) {
-	}
-	void func() const {
-		//b.funB();
-
-		cout << typeid(b).name();
-	}
-
-private:
-	B b;
-};
 
 int main() {
-	A a;
-	a.func();
 }
