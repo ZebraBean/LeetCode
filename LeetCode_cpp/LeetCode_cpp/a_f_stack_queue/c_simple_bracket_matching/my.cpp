151c540bc2628bbcc379a05d22dd8e1baaf1b41a
Zebra <1349889154@qq.com>
2022-10-26 11:13:16 +0800

    solve the problem brace matching

---
+++ LeetCode_cpp/LeetCode_cpp/a_f_stack_queue/c_simple_bracket_matching/my.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<stack>
+using namespace std;
+class Solution {
+public:
+	bool isValid(string s) {
+		stack<char> st;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+				st.push(s[i]);
+			} else {
+				if (st.empty()) {
+					return false;
+				}
+				char tmp = st.top();
+				st.pop();
+				if (s[i] == ')' && tmp != '(' || s[i] == '}' && tmp != '{'
+					|| s[i] == ']' && tmp != '[') {
+					return false;
+				}
+			}
+		}
+		if (!st.empty()) {
+			return false;
+		}
+		return true;
+	}
+};
